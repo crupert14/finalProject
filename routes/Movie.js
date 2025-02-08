e5b282057b06d6283f6bf82d8efca06aa1d0f4c2
Cade Rupert <121654954+crupert14@users.noreply.github.com>
2025-02-08 16:21:16 -0500

    working movie page

---
+++ routes/Movie.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', async (req, res) => {
+    const response = await fetch(`https://www.omdbapi.com/?t=hjkhjk&apikey=${process.env.OMDB_KEY}`);
+    const data = await response.json();
+  
+    console.log(data);
+  
+    //img: data["Search"][7].Poster, **Only works when an array of JSON objects is returned using the s= parameter
+
+    if(data.Response != 'False') {
+        res.render('movieView.ejs', {
+        img: data.Poster,
+        title: data.Title,
+        year: data.Year,
+        rated: data.Rated,
+        released: data.Released,
+        runtime: data.Runtime,
+        genre: data.Genre,
+        summary: data.Plot,
+        cast: data.Actors,
+        director: data.Director,
+        writers: data.Writer,
+        languages: data.Language,
+        country: data.Country,
+        awards: data.Awards,
+        imdbrating: data.imdbRating,
+        rt: data.Ratings[1].Value,
+        meta: data.Metascore,
+        boxoffice: data.BoxOffice
+        });
+    }
+    else {
+        res.send(data.Error);
+    }
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,23 +7,14 @@ require('dotenv').config()
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
+const movieViewRoute = require('./routes/Movie');
+
+app.use('/MovieInfo', movieViewRoute);
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-app.get('/', async (req, res) => {
-    const response = await fetch(`https://www.omdbapi.com/?t=bladerunner&apikey=${process.env.OMDB_KEY}`);
-    const data = await response.json();
-
-    console.log(data);
-
-    res.render('sample.ejs', {
-      img: data.Poster,
-      title: data.Title,
-      year: data.Year,
-      rated: data.Rated,
-      released: data.Released,
-      runtime: data.Runtime,
-      genre: data.Genre
-    });
-});
+app.get('/', (req, res) => {
+  res.send("in progress...")
+})
